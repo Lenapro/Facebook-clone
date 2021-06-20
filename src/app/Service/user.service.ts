038ab11324338c6fa745b775d30596f567c06d35
Lenapro <helenapoaty7@gmail.com>
2021-06-20 18:41:37 +0200

    version

--- src/app/Service/user.service.ts
+++ src/app/Service/user.service.ts
@@ -8,6 +8,10 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor(private http:HttpClient) { }
+   data ={
+    "login": "",
+    "password": ""
+  }
 
   inscription(data) {
     const headers = { 'Content-Type': 'application/json' };
@@ -17,6 +21,12 @@ export class UserService {
   get_user(id) {
     return this.http.get("http://localhost:8000/facebook/get/" +id);
   }
-
+  connexion_user(email, password){
+    this.data.login = email;
+    this.data.password = password;
+    const header = { 'Content-Type': 'application/json' };
+    const body = JSON.stringify(this.data);
+    return this.http.post("http://localhost:8000/facebook/connexion", body, { 'headers': header } )
+  }
   
 }
