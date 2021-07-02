05332d6a87e1bd6a118d46f574ad6dacebcbce5e
Lenapro <helenapoaty7@gmail.com>
2021-07-02 19:45:17 +0200

    version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { InscriptionComponent } from './inscription/inscription.component';
 import{AccueilComponent} from './accueil/accueil.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path : 'connexion', component : ConnexionComponent},
   { path : 'inscription', component : InscriptionComponent},
   {path : 'accueil', component : AccueilComponent},
-  { path : '', redirectTo: 'connexion', pathMatch:'full'}
+  { path : '', redirectTo: 'connexion', pathMatch:'full'},
+  {path : 'profile', component : ProfileComponent }
 ];
 
 
